757a26fac7375bf81f5862546f262993f490bb5d
tiagodiasbr <69697797+tiagodiasbr@users.noreply.github.com>
2020-08-14 22:14:55 -0300

    Update Form1.vb

--- Teste Windows/Teste Windows/Form1.vb
+++ Teste Windows/Teste Windows/Form1.vb
@@ -4,7 +4,7 @@
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        MsgBox("PIG")
+        MsgBox("PIG2")
     End Sub
     
     'alterei pelo git
